3050caea2f73b3979cdeb758fcbba8d588576030
Andres Valencia <andresv2@mac.com>
2015-03-22 21:52:20 -0400

    updates changes to gemfile

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 require 'httparty'
-
+# require 'flickr'
 class HomeController < ApplicationController
 
   def index
@@ -8,6 +8,18 @@ class HomeController < ApplicationController
 
     # Quote to show is inside response.contents.quote
     @quoteStr = response['quoteText']
+
+   # @flickr_api= '1f012f3e9cc5b28d42ebfe203097107f'
+   # @flickr_secret='761c7a82a443a6bc'
+
+   # flickr = Flickr.new(@flickr_api)
+   # params = Hash.new()
+   # params[:safe_search] = 3
+   # params[:format] = "json"
+   # params[:tags] = "moon"
+
+   # photos = flickr.photos_search(params)
+   # # $stdout.puts(photos)
   end
 
   def show
